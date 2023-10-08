36a0d3a017e98c8bf0a102300cc108969d9199ca
freedomosato <114444490+freedomosato@users.noreply.github.com>
2023-10-08 12:57:10 +0000

    introduce if statement in .js to reduce the number of functions

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
  let resultEl = document.getElementById("result")
 
-function calculate(number){
-  resultEl.innerHTML += number
+function calculate(num){
+    resultEl.innerHTML += num
 }
-function equal(){
-  try{
-      resultEl.innerHTML = eval(resultEl.innerHTML)
-  }
-  catch(err){
-    alert("invalid input")
-    
-  }
-}
-
-function clr(){
-  resultEl.innerHTML = ""
-}
-
-function del(){
-  resultEl.innerHTML = resultEl.innerHTML.slice(0, -1)
-}
-
-function percent(){
-  resultEl.innerHTML = eval(resultEl.innerHTML/100)
-}
+ function cal(character){
+     if(character === 'Del'){
+         resultEl.innerHTML = resultEl.innerHTML.slice(0, -1)
+         
+     }else if(character === 'Cls'){   
+         resultEl.innerHTML = null
+         resultEl.ariaPlaceholder = '0.' 
+         
+     }else if(character === '%'){
+         resultEl.innerHTML = resultEl.innerHTML/100
+       
+     }else if(character === '='){
+         try{
+        resultEl.innerHTML = eval(resultEl.innerHTML)
+        }
+        catch(err){
+        resultEl.innerHTML = "invalid input"
+        }
+     }
+ }
